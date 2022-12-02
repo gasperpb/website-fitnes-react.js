9cafabdc7feeac377d044d0d0ae5f946417b2223
gasperpb <achillespb@hotmail.com>
2022-12-02 12:02:09 -0300

    Navbar etapa 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,33 +5,49 @@ import { FiMenu } from 'react-icons/fi';
 import { BiWorld } from "react-icons/bi"
 import { BsCart2 } from "react-icons/bs"
 import { TfiUser } from "react-icons/tfi"
+import logo from "../assets/logo.png"
 const Navbar = () => {
-  return <div className="">
+  return (<div>
     {/* Add */}
-
-    <div className="flex sm:hidden">
-      <img src={addmobile} alt="" />
-    </div>
-    {/* left */}
-
-    <div className="hidden sm:flex">
-      <img src={addweb} alt="" />
-    </div>
-    <div className="bg-[#005599] text-white">
-      <FiMenu className="w-12 h-10" />
-      <BiWorld className="w-12 h-10" />
-    </div>
-    {/* Midle */}
-
-    {/* Right */}
-
-    <div className=" text-white">
-      <BsCart2 className="w-9 h-9" />
+    <div className=" bg-[#005599] flex justify-center">
+      <div className="flex sm:hidden mx-auto">
+        <img src={addmobile} alt="" />
+      </div>
+      <div className="hidden sm:flex mx-auto">
+        <img src={addweb} alt="" />
+      </div>
     </div>
 
+    {/* Navbar */}
 
+    <div className="bg-[#005599] flex items-center justify-between px-3">
+      {/* left */}
+      <div className=" text-white flex items-center">
+        <FiMenu className="w-12 h-10" />
+        <BiWorld className="w-12 h-10" />
+      </div>
 
-  </div>;
+      {/* Middle */}
+
+      <div className=" flex items-center w-20 h-20">
+        <img src={logo} alt="" />
+      </div>
+      <div className="items-center hidden md:flex">
+        <input
+          type="search"
+          className="rounded-[0.3rem] py-1 w-[25rem] lg:w-[35rem]"
+        />
+      </div>
+
+      {/* Right */}
+
+      <div className=" text-white flex items-center gap-3">
+        <BsCart2 className="w-8 h-8" />
+        <TfiUser className="w-8 h-8" />
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Navbar;
